896ec7422e2fb746a903d7a2a424cc042c5bf1df
Koczo Koczownik <mateux111@gmail.com>
2020-11-10 16:47:10 +0100

    binary gene fix

--- GA/Data/Chromosome.cs
+++ GA/Data/Chromosome.cs
@@ -1,21 +1,24 @@
-﻿namespace GA1
+﻿using System;
+
+namespace GA1
 {
     public class Chromosome
     {
         private int _gene;
+
         public int Gene
         {
             get
             {
                 if (_gene < 0) return 0; //check for any incorrect
-                return _gene >= ChromosomeDefinition.PossibleLargestChromosome 
-                    ? ChromosomeDefinition.PossibleLargestChromosome 
+                return _gene >= ChromosomeDefinition.PossibleLargestChromosome
+                    ? ChromosomeDefinition.PossibleLargestChromosome
                     : _gene;
             }
             set => _gene = value;
         }
 
         public double GeneInDecimal() => ResearchDefinitions.GetElementOfNPosition(Gene);
-        public string GeneInBinary() => 
-    }
+        public string GeneInBinary() => ChromosomeDefinition.BinaryGeneFix(Convert.ToString(Gene, 2));
+}
 }
