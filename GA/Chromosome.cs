5e850fdcb953bc47216129f2c535ca02e7bc2ca9
Koczo Koczownik <mateux111@gmail.com>
2020-11-10 14:33:58 +0100

    Add project files.

---
+++ GA/Chromosome.cs
@@ -0,0 +1,21 @@
+﻿namespace GA1
+{
+    public class Chromosome
+    {
+        private int _gene;
+        public int Gene
+        {
+            get
+            {
+                if (_gene < 0) return 0; //check for any incorrect
+                return _gene >= ChromosomeDefinition.PossibleLargestChromosome 
+                    ? ChromosomeDefinition.PossibleLargestChromosome 
+                    : _gene;
+            }
+            set => _gene = value;
+        }
+
+        public double GeneInDecimal() => ResearchDefinitions.GetElementOfNPosition(Gene);
+        public string GeneInBinary() => 
+    }
+}
---
+++ GATests/ChromosomeTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using GA1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GA1.Tests
+{
+    public class ChromosomeTests
+    {
+        [Fact()]
+        public void GeneInDecimalTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
